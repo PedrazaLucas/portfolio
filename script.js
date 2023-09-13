80bca3eeddb447b580cac8c96bd2dc75cd23f17c
LucasPedraza <pedrazaalucas@gmail.com>
2023-09-13 00:10:27 -0300

    actu2 12/09

--- script.js
+++ script.js
@@ -9,21 +9,25 @@ while(vidaSoldado1>0 && vidaSoldado2>0){
     let disparoSoldado1 = parseInt(Math.random()*20)
     let disparoSoldado2 = parseInt(Math.random()*20)
 
-console.log("El Soldado 1 le saca " + disparoSoldado1 + " de vida al Soldado2" )
-console.log("El Soldado 2 le saca " + disparoSoldado2 + " de vida al Soldado1" )
+ console.log("Soldado A dispara un: "+ disparoSoldado1)
+console.log("Soldado B dispara un: "+ disparoSoldado2)
 
 
 
 
 if(disparoSoldado1>disparoSoldado2){
     vidaSoldado2 -= disparoSoldado1
-    console.log("El Soldado 1 le saca " + disparoSoldado1 + " de vida al soldado 2")
-    console.log("El soldado 2 queda con " + vidaSoldado2 + " de vida")
+    console.log("El Soldado A le saca " + disparoSoldado1 + " de vida al soldado B")
+    console.log("El soldado B queda con " + vidaSoldado2 + " de vida")
 }else if(disparoSoldado2>disparoSoldado1){
     vidaSoldado1 -= disparoSoldado2
-    console.log("El Soldado 2 le saca " + disparoSoldado1 + " de vida al soldado 1")
-    console.log("El soldado 1 queda con " + vidaSoldado1 + " de vida")
+    console.log("El Soldado B le saca " + disparoSoldado2 + " de vida al soldado A")
+    console.log("El soldado A queda con " + vidaSoldado1 + " de vida")
+}else if(disparoSoldado1=disparoSoldado2){
+    console.log("No se sacaron vida")
 }
 
+console.log("--------------------------------------")
 
 }
+
